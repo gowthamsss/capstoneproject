e3e9ea67cc309bfc22ce3ff95146a8b2af9817fd
gowthamsss <52155234+gowthamsss@users.noreply.github.com>
2022-06-14 20:25:06 +0530

    Add files via upload

---
+++ RatingStudents/src/main/java/com/wipro/RatingStudents/RatingStudentsApplication.java
@@ -0,0 +1,12 @@
+package com.wipro.RatingStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingStudentsApplication.class, args);
+	}
+}
